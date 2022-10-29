c36a76ebcfcaec233779aea1ae8e30973d7ec4ad
szejnaArtur <50076342+szejnaArtur@users.noreply.github.com>
2022-10-29 11:14:13 +0200

    Spring boot project initialization.

---
+++ src/main/java/pl/backend/flashcardapp/FlashcardAppApplication.java
@@ -0,0 +1,13 @@
+package pl.backend.flashcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/backend/flashcardapp/FlashcardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.backend.flashcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
